7397eba8aa03fc3966488e873d1dc0e0ca5aeedc
carmcc <carmine.marchesani@studenti.unicam.it>
2023-05-02 21:52:52 +0200

    code cleanup

--- backend/src/it/unicam/cs/pawm/index.js
+++ backend/src/it/unicam/cs/pawm/index.js
@@ -1,28 +1,22 @@
 const express = require('express')
-const router = express.Router()
-//controller
-const userController = require('./routes/userController')
-const authController = require('./routes/auth')
-//models
-const user = require('./models/user')
-
 const app = express()
 
-const port = process.env.PORT || 3000
+const userRoutes = require('./routes/userRoute')
+const authRoutes = require('./routes/authRoute')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/users', userController);
-app.use('/auth', authController);
+app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
 
 const db = require('./config/db')
-
 db.sync()
     .then(() => { console.log("Connected."); })
     .catch((err) => { console.log("Not Connected : " + err.message);
 });
 
-
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`App listening on port ${port}`))
 
 
