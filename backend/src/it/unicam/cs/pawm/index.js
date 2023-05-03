6b990717ff3741d1001a52625cec0efb96c3517e
carmcc <carmine.marchesani@studenti.unicam.it>
2023-05-03 15:39:27 +0200

    minor edits

--- backend/src/it/unicam/cs/pawm/index.js
+++ backend/src/it/unicam/cs/pawm/index.js
@@ -2,13 +2,11 @@ const express = require('express')
 const app = express()
 
 const userRoutes = require('./routes/userRoute')
-const authRoutes = require('./routes/authRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/users', userRoutes);
-app.use('/auth', authRoutes);
+app.use(userRoutes);
 
 const db = require('./config/db')
 db.sync()
@@ -16,7 +14,7 @@ db.sync()
     .catch((err) => { console.log("Not Connected : " + err.message);
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3090
 app.listen(port, () => console.log(`App listening on port ${port}`))
 
 
