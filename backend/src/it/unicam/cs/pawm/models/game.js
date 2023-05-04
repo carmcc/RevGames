af0c89f31809fcc9b6854de1838c45c36d0e69fb
carmcc <carmine.marchesani@studenti.unicam.it>
2023-05-04 16:08:17 +0200

    [ADD] added Game model

---
+++ backend/src/it/unicam/cs/pawm/models/game.js
@@ -0,0 +1,40 @@
+const { DataTypes, Model} = require('sequelize');
+const sequelize = require('../config/db');
+
+class Game extends Model {}
+
+Game.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+    }
+    ,
+    {
+        sequelize,
+        modelName: "Games",
+        timestamps: false,
+        force: true
+    });
+
+//TODO inserire le associazioni con la tabella Users
+
+module.exports = Game;
+
+
