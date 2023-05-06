fd80ae9d2d1da929522b61d8a5629ea704a54e97
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-05-06 22:29:04 +0200

    [ADD] add project vue.js and implements first function Home.vue, ComponentFooter.vue, SignIn.vue, SignUp.vue, NavSideBar.vue

---
+++ frontend/scoregame/src/components/SignIn.vue
@@ -0,0 +1,96 @@
+<template>
+  <form @submit.prevent="loginUser">
+    <!--    <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">-->
+    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+    <div>
+      <label>Username:</label>
+      <input type="text" v-model="username" required>
+    </div>
+    <div>
+      <label>Password:</label>
+      <input type="password" v-model="password" required>
+    </div>
+    <!--      <div>-->
+    <!--        <button type="submit">Registrati</button>-->
+    <!--      </div>-->
+    <!--    <div class="checkbox mb-3">-->
+    <!--      <label>-->
+    <!--        <input type="checkbox" value="remember-me"> Remember me-->
+    <!--      </label>-->
+    <!--    </div>-->
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'SignIn',
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginUser() {
+      axios.post('http://localhost:3000/auth/login', {
+        username: this.username,
+        password: this.password
+      })
+          .then(response => {
+            // Gestisci la risposta dal server se la login ha avuto successo
+            const responseData = {message: 'Login effettuata con successo', data: response.data};
+            console.log(responseData);
+          })
+          .catch(error => {
+            // Gestisci l'errore in caso di fallimento della login
+            console.log(this.username,this.password);
+            // console.log({data:error.response.data});
+            console.log(error);
+          });
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+
+label {
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  width: 100%;
+  max-width: 400px;
+}
+
+button[type="submit"] {
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+button[type="submit"]:hover {
+  background-color: #0062cc;
+}
+
+</style>
