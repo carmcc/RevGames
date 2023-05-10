2858b62c60c6125a3b268bf14147a691bee4b1b7
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-05-10 17:15:06 +0200

    [UPDATE] configuration for store.js

--- frontend/revgames/src/main.js
+++ frontend/revgames/src/main.js
@@ -1,8 +1,21 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// import store from './store'
+// import './axios'
+//
+// createApp(App).use(router).mount('#app')
+//
+// new Vue({
+//     el: '#app',
+//     store,
+//     render: h => h(App)
+// })
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import './axios'
 
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
