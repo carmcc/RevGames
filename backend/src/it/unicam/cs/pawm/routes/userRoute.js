81ca2243f06d8e56f9a4075d91dbedd75c837774
carmcc <carmine.marchesani@studenti.unicam.it>
2023-05-10 23:14:43 +0200

    [ADD] added refresh token validation

--- backend/src/it/unicam/cs/pawm/routes/userRoute.js
+++ backend/src/it/unicam/cs/pawm/routes/userRoute.js
@@ -2,12 +2,14 @@ const express= require('express')
 const router = express.Router()
 
 const userController = require('../controllers/userController')
+
 router.post('/auth/login', userController.login)
 router.post('/auth/register', userController.register)
 router.post('/auth/logout', userController.logout)
 router.get('/get-nonce', userController.getNonce)
 router.get('/api/protected', userController.protectedRoute)
 router.get('/users/allUsers', userController.getAllUsers)
+router.get('/verify-refreshToken', userController.verifyRefreshToken)
 // router.get('/users/username', userController.getUserByUsername)
 
 //modulo da esportare per le rotte, richiamato in index.js
