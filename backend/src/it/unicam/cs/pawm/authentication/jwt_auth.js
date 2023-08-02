430ee5a09f72e063b5d5088911af406d3405ff57
carmcc <carmine.marchesani@studenti.unicam.it>
2023-08-02 16:42:11 +0200

    [Update] Duplicated code removed and added method for generic token type validation

--- backend/src/it/unicam/cs/pawm/authentication/jwt_auth.js
+++ backend/src/it/unicam/cs/pawm/authentication/jwt_auth.js
@@ -1,7 +1,11 @@
 const jwt_auth = require('jsonwebtoken')
 require('dotenv').config()
 
-let invalidToken = [];
+/**
+ * Array of invalid tokens
+ * @type {*[]}
+ */
+let invalidTokens = [];
 
 /**
  * Generates an access token
@@ -22,30 +26,13 @@ function generateRefreshToken(payload) {
 }
 
 /**
- * Verifies if the token is valid and not expired
+ * Verifies if the access token is valid and not expired
  * @param req
  * @param res
  * @param next
  */
 function verifyAccessToken(req, res, next) {
-    const authHeader = req.header('Authorization');
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if(!token) return res.status(401).send({message:'Unauthorized. Token not found', status: 401});
-
-    //se il token è presente nell'array dei token invalidi, nego l'accesso
-    if (invalidToken.includes(token)) {
-        return res.status(403).send({message: 'Forbidden Access. Token is not valid because it is expired', status: 403});
-    }
-
-    //verifico il token e lo decodifico, passando alla callback l'eventuale errore e l'utente
-    jwt_auth.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err)
-            return res.status(403).send({message: 'Forbidden Access. Token is not valid', status: 403});
-        req.user = user;
-        //passo il controllo al prossimo middleware cioe alla funzione protectedRoute()
-        next();
-    })
+    verifyToken(req,res,next, 'access');
 }
 
 
@@ -55,28 +42,13 @@ function verifyAccessToken(req, res, next) {
  * @param res
  * @param next
  */
-function verifyRefreshToken(req, res, next)
-{
-    const authHeader = req.header('Authorization');
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if(!token) return res.status(401).send({message:'Unauthorized. Token not found', status: 401});
-
-    //se il token è presente nell'array dei token invalidi, nego l'accesso
-    if (invalidToken.includes(token)) {
-        return res.status(403).send({message: 'Forbidden Access. Token is not valid because it is expired', status: 403});
-    }
-    jwt_auth.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err)
-            return res.status(403).send({message: 'Forbidden Access. Token is not valid', status: 403});
-        req.user = user;
-        next();
-    })
+function verifyRefreshToken(req, res, next){
+    verifyToken(req,res,next, 'refresh');
 }
 
 
 /**
- * Invalidates a token
+ * Invalidates an access token
  * @param token the token to invalidate
  * @returns {Promise<boolean>} true if the token is invalidated, false otherwise
  */
@@ -86,8 +58,8 @@ async function invalidateAccessToken(token) {
         await jwt_auth.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         //se il token non è presente nell'array dei token invalidi, lo aggiungo per invalidarlo
-        if(!invalidToken.includes(token))
-            invalidToken.push(token);
+        if(!invalidTokens.includes(token))
+            invalidTokens.push(token);
         //senza return true, il metodo non si conclude e non viene invalidato il token
         return true;
     } catch (error) {
@@ -104,12 +76,9 @@ async function invalidateAccessToken(token) {
 async function invalidateRefreshToken(token)
 {
     try {
-        // verifico se il token è valido
         await jwt_auth.verify(token, process.env.REFRESH_TOKEN_SECRET);
-
-        //se il token non è presente nell'array dei token invalidi, lo aggiungo per invalidarlo
-        if(!invalidToken.includes(token))
-            invalidToken.push(token);
+        if(!invalidTokens.includes(token))
+            invalidTokens.push(token);
         return true;
     }
     catch (error) {
@@ -118,6 +87,32 @@ async function invalidateRefreshToken(token)
     }
 }
 
+/**
+ * Verifies if the token is valid and not expired
+ * @param req
+ * @param res
+ * @param next
+ * @param tokenType
+ * @returns {*}
+ */
+function verifyToken(req,res,next, tokenType) {
+    const authHeader = req.header('Authorization');
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token) return res.status(401).send({message:'Unauthorized. Token not found', status: 401});
+
+    if (invalidTokens.includes(token)) {
+        return res.status(403).send({message: 'Forbidden Access. Token is not valid because it is expired', status: 403});
+    }
+    const secret = tokenType === 'access' ? process.env.ACCESS_TOKEN_SECRET : process.env.REFRESH_TOKEN_SECRET;
+
+    jwt_auth.verify(token, secret, (err, user) => {
+        if (err)
+            return res.status(403).send({message: 'Forbidden Access. Token is not valid', status: 403});
+        req.user = user;
+        next();
+    })
+}
 
 
 module.exports = {generateAccessToken, generateRefreshToken, verifyAccessToken, verifyRefreshToken, invalidateAccessToken, invalidateRefreshToken}
